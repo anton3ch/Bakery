9d7f918783cc6360f0977a878f0ac006e122c080
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-12-09 12:09:32 -0600

    pass the 2nd test

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -4,11 +4,11 @@ namespace Bakery
   {
     public int Price { get; set; } = 5;
 
-    public int (int quantity)
+    public int TotalCost(int quantity)
     {
       int finalCost = 0;
-      if (quantity == 1) {
-        finalCost += Price;
+      if (quantity < 3) {
+        finalCost += Price * quantity;
       }
       return finalCost;
     }
