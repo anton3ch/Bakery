60ab32ea6996c7c7f0d227382cdba10a9ccf7feb
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-12-09 16:29:54 -0600

    add branchinh statement for ordering bread

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -17,42 +17,63 @@ namespace Bakery
       Console.WriteLine("Bread: Buy 2, get 1 free.");
       Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5.");
       Console.WriteLine("----------------------");
-      Console.WriteLine("Would you like to place an order? (Yes/No):");
-      string answer = Console.ReadLine();
-      answer = answer.ToLower();
-
-      if(answer == "yes") 
+      bool repeat = true;
+      while(repeat)
       {
-        Console.WriteLine("Do you wanna buy bread?(Y/N)");
-        string breadBuy = Console.ReadLine();
-        breadBuy = breadBuy.ToLower();
-        if(breadBuy == "yes")
-        {
-          Console.WriteLine("----------------------");
-          Console.WriteLine("The special is Buy 2, get 1 free.");
-          Console.WriteLine("How many would you like?");
-          string breadQuantityStr = Console.ReadLine();
-          int breadQuantity = Int32.Parse(breadQuantityStr);
-        }
-        Console.WriteLine("----------------------");
-        Console.WriteLine("Do you wanna buy pastry?(Y/N)");
-        string pastryBuy = Console.ReadLine();
-        pastryBuy = pastryBuy.ToLower();
-        
-        if(pastryBuy == "yes")
-        {
-          Console.WriteLine("----------------------");
-          Console.WriteLine("The special is Buy 1 for $2 or 3 for $5.");
-          Console.WriteLine("How many would you like?");
-          string pastryQuantityStr = Console.ReadLine();
-          int pastryQuantity = Int32.Parse(pastryQuantityStr);
-        }
+        Console.WriteLine("Would you like to place an order? (Yes/No):");
+        string answer = Console.ReadLine();
+        answer = answer.ToLower();
 
-      } else if (answer == "no") 
-      {
-        Console.WriteLine("Thank you for visiting! Come again soon!");
-      } else {
-        Console.WriteLine("Wrong input. Please, enter 'yes' or 'no'!");
+        if(answer == "yes") 
+        {
+          int breadQuantity;
+          int pastryQuantity;
+          bool breadLoop = true;
+          bool pastryLoop = true;
+          while(breadLoop)
+          {
+            Console.WriteLine("Do you wanna buy bread?(Y/N)");
+            string breadBuy = Console.ReadLine();
+            breadBuy = breadBuy.ToLower();
+            if(breadBuy == "y")
+            {
+              Console.WriteLine("----------------------");
+              Console.WriteLine("The special is Buy 2, get 1 free.");
+              Console.WriteLine("How many would you like?");
+              string breadQuantityStr = Console.ReadLine();
+              breadQuantity = Int32.Parse(breadQuantityStr);
+              breadLoop = false;
+            } else if (breadBuy == "n")
+            {
+              breadLoop = false;
+            } else if (breadBuy != "n")
+            {
+              Console.WriteLine("Wrong input. Please, enter 'y' for 'yes' or 'n' for 'no'!");
+              breadLoop = true;
+            }
+          }
+
+          Console.WriteLine("----------------------");
+          Console.WriteLine("Do you wanna buy pastry?(Y/N)");
+          string pastryBuy = Console.ReadLine();
+          pastryBuy = pastryBuy.ToLower();
+
+          if(pastryBuy == "y")
+          {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("The special is Buy 1 for $2 or 3 for $5.");
+            Console.WriteLine("How many would you like?");
+            string pastryQuantityStr = Console.ReadLine();
+            pastryQuantity = Int32.Parse(pastryQuantityStr);
+          }
+
+        } else if (answer == "no") 
+        {
+          Console.WriteLine("Thank you for visiting! Come again soon!");
+          repeat = false;
+        } else {
+          Console.WriteLine("Wrong input. Please, enter 'yes' or 'no'!");
+        }
       }
     }
   }
