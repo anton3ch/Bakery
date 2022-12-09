3217f29acde8ac8380faed41683e47d9e5d10368
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-12-09 16:48:13 -0600

    refractor tests for pastry

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -9,29 +9,25 @@ namespace Bakery.Tests
     [TestMethod]
     public void TotalCost_ShouldReturnTotalCostOfOnePastry_PriceForOnePastry()
     {
-      Pastry testPastry = new Pastry();
-      Assert.AreEqual(2, testPastry.TotalCost(1));
+      Assert.AreEqual(2, Pastry.TotalCost(1));
     }
 
     [TestMethod]
     public void TotalCost_ShouldReturnTotalCostOfTwoPastries_PriceForTwoPastries()
     {
-      Pastry testPastry = new Pastry();
-      Assert.AreEqual(4, testPastry.TotalCost(2));
+      Assert.AreEqual(4, Pastry.TotalCost(2));
     }
 
     [TestMethod]
     public void TotalCost_ShouldReturnTotalCostOfThreePastries_PriceForThreePastries()
     {
-      Pastry testPastry = new Pastry();
-      Assert.AreEqual(5, testPastry.TotalCost(3));
+      Assert.AreEqual(5, Pastry.TotalCost(3));
     }
 
     [TestMethod]
     public void TotalCost_ShouldReturnTotalCostOfTenPastries_PriceForTenPastries()
     {
-      Pastry testPastry = new Pastry();
-      Assert.AreEqual(17, testPastry.TotalCost(10));
+      Assert.AreEqual(17, Pastry.TotalCost(10));
     }
   }
 }
